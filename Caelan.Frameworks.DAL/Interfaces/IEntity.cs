50383c86cc5f3eae9d78bb51f7bd5b9cc06f1b34
Matteo Bruni <Ar3sHack@gmail.com>
2014-05-26 12:55:30 +0200

    Initial commit

---
+++ Caelan.Frameworks.DAL/Interfaces/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Caelan.Frameworks.DAL.Interfaces
+{
+	public interface IEntity
+	{
+	}
+
+	public interface IEntity<out T> : IEntity where T : IEquatable<T>
+	{
+		T ID { get; }
+	}
+}
