619ebc346f501e253265fa8e5187cc867ac5aca4
Matteo Bruni <matteo.bruni@me.com>
2014-06-23 13:31:21 +0200

    Migration to F#

---
+++ Caelan.Frameworks.DAL/Interfaces.fs
@@ -0,0 +1,6 @@
+﻿namespace Caelan.Frameworks.DAL.Interfaces
+    type IEntity = interface end
+
+    type IEntity<'TKey when 'TKey :> System.IEquatable<'TKey>> =
+        inherit IEntity
+        abstract member ID : 'TKey with get, set
---
+++ Caelan.Frameworks.DAL/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "IEntity.fs"
+open Caelan.Frameworks.DAL
+
+// Define your library scripting code here
+
