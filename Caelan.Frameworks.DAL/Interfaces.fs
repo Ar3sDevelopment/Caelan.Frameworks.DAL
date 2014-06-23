b07a7a7d863d29a43d930406d33b70a043cdedaa
Matteo Bruni <matteo.bruni@me.com>
2014-06-23 19:55:29 +0200

    Alignment with c# implementations

--- Caelan.Frameworks.DAL/Interfaces.fs
+++ Caelan.Frameworks.DAL/Interfaces.fs
@@ -4,3 +4,11 @@
     type IEntity<'TKey when 'TKey :> System.IEquatable<'TKey>> =
         inherit IEntity
         abstract member ID : 'TKey with get, set
+
+    type ISet<'TEntity when 'TEntity :> IEntity> =
+        abstract member Insert : 'TEntity -> bool
+        abstract member Update : 'TEntity -> bool
+        abstract member Delete : 'TEntity -> bool
+
+    type IContext =
+        abstract member Save : unit -> int
