8a8a5c55f71efe4f3d2c1139acb5300cbe34d672
Matteo Bruni <matteo.bruni@me.com>
2014-06-24 18:15:21 +0200

    Added AssemblyInfo

--- Caelan.Frameworks.DAL/Script.fsx
+++ Caelan.Frameworks.DAL/Script.fsx
@@ -1,8 +1,1 @@
-﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "IEntity.fs"
-open Caelan.Frameworks.DAL
-
-// Define your library scripting code here
-
+﻿System.Guid.NewGuid()
