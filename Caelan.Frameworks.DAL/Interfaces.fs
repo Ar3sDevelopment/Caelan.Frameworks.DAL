5fc75ddc1cc77d0843e793d7f3735319d8e0bc3c
Matteo Bruni <matteo.bruni@me.com>
2014-06-25 14:49:21 +0200

    Removed some abstraction

--- Caelan.Frameworks.DAL/Interfaces.fs
+++ Caelan.Frameworks.DAL/Interfaces.fs
@@ -5,11 +5,12 @@
         inherit IEntity
         abstract member ID : 'TKey with get, set
 
-    type ISet<'TEntity when 'TEntity :> IEntity> =
-        inherit System.Linq.IQueryable<'TEntity>
-        abstract member Insert : 'TEntity -> bool
-        abstract member Update : 'TEntity -> bool
-        abstract member Delete : 'TEntity -> bool
-
-    type IContext =
-        abstract member Save : unit -> int
+//TODO: Abstraction at this level will be implemented later
+//    type ISet<'TEntity when 'TEntity :> IEntity> =
+//        inherit System.Linq.IQueryable<'TEntity>
+//        abstract member Insert : 'TEntity -> bool
+//        abstract member Update : 'TEntity -> bool
+//        abstract member Delete : 'TEntity -> bool
+//
+//    type IContext =
+//        abstract member Save : unit -> int
